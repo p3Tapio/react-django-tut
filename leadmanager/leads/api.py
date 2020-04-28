ccc5b757abc2e3fd8086bb161f55df1d44563651
p3Tapio <55560071+p3Tapio@users.noreply.github.com>
2020-04-29 01:13:02 +0800

    Update api.py

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -2,7 +2,7 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
-# Lead Viewset (täys crudi ilman metodien specifiointia ! :D )
+# Lead Viewset = koko CRUD
 
 class LeadViewSet(viewsets.ModelViewSet):
     permission_classes = [
